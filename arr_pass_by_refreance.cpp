1b864f20b67ff237e76fd59099ca1a55115b1d61
studentmegha <meghayadav8848488448@gmail.com>
2025-12-23 09:25:44 -0800

    refrence code

---
+++ arr_pass_by_refreance.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void changeArr(int arr[] , int size){
+  cout << "in function\n" ;
+  for (int i =0 ; i<size ; i++){
+    arr[i] = 2*arr[i];
+  }
+}
+int main(){
+  int arr[] = {1, 2, 3};
+  changeArr(arr , 3);
+  cout << "\n in main\n";
+  for (int i =0; i<3; i++){  //2 4 6
+    cout << arr[i] << " " ;
+ }
+  cout << endl;
+  return 0;
+}
